837d280dfb91d8a58c5013560150b3f048e3f66c
Fabio Biasi Mello Rubim <fabiorubim@hotmail.com>
2022-06-15 01:30:52 -0300

    Refatoração e página de novos produtos em Go.

--- models/produtos.go
+++ models/produtos.go
@@ -44,3 +44,13 @@ func BuscaTodosOsProdutos() []Produto {
 	defer db.Close()
 	return produtos
 }
+
+func CriarNovoProduto(nome, descricao string, preco float64, quantidade int) {
+	db := db.ConectaComBancoDeDados()
+	insereDadosNoBanco, err := db.Prepare("INSERT INTO produtos(nome, descricao, preco, quantidade) VALUES($1, $2, $3, $4)")
+	if err != nil {
+		panic(err.Error())
+	}
+	insereDadosNoBanco.Exec(nome, descricao, preco, quantidade)
+	defer db.Close()
+}
--- routes/controllers/produtos.go
+++ routes/controllers/produtos.go
@@ -1,8 +1,10 @@
 package controllers
 
 import (
+	"log"
 	"loja/models"
 	"net/http"
+	"strconv"
 	"text/template"
 )
 
@@ -11,4 +13,30 @@ var temp = template.Must(template.ParseGlob("templates/*.html"))
 func Index(w http.ResponseWriter, r *http.Request) {
 	todosOsProdutos := models.BuscaTodosOsProdutos()
 	temp.ExecuteTemplate(w, "Index", todosOsProdutos)
+}
+
+func New(w http.ResponseWriter, r *http.Request) {
+	temp.ExecuteTemplate(w, "New", nil)
+}
+
+func Insert(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		nome := r.FormValue("nome")
+		descricao := r.FormValue("descricao")
+		preco := r.FormValue("preco")
+		quantidade := r.FormValue("quantidade")
+
+		precoConvertidoParaFloat, err := strconv.ParseFloat(preco, 64)
+		if err != nil {
+			log.Println("Erro na conversão do preço: ", err)
+		}
+
+		quantidadeConvetidaParaInt, err := strconv.Atoi(quantidade)
+		if err != nil {
+			log.Println("Erro na conversão da quantidade: ", err)
+		}
+
+		models.CriarNovoProduto(nome, descricao, precoConvertidoParaFloat, quantidadeConvetidaParaInt)
+		http.Redirect(w, r, "/", 301)
+	}
 }
