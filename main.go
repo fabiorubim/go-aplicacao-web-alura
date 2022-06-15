2785c31d997d75cf850c80a3da160c54ab47748d
Fabio Biasi Mello Rubim <fabiorubim@hotmail.com>
2022-06-15 00:50:27 -0300

    Conectando ao banco de dados postgresql utilizando Go.

--- main.go
+++ main.go
@@ -1,11 +1,26 @@
 package main
 
 import (
+	"database/sql"
+	"log"
 	"net/http"
 	"text/template"
+
+	_ "github.com/lib/pq"
 )
 
+func conectaComBancoDeDados() *sql.DB {
+	conexao := "user=postgres dbname=alura_loja password=root sslmode=disable host=localhost"
+	db, err := sql.Open("postgres", conexao)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return db
+}
+
 type Produto struct {
+	id         int
 	Nome       string
 	Descricao  string
 	Preco      float64
@@ -20,14 +35,45 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	produtos := []Produto{
-		{Nome: "Camiseta",
-			Descricao:  "Azul, bem bonita",
-			Preco:      39,
-			Quantidade: 10},
-		{"Tenis", "Conforatável", 89, 3},
-		{"Fone", "Muito bom", 59, 2},
+	// produtos := []Produto{
+	// 	{Nome: "Camiseta",
+	// 		Descricao:  "Azul, bem bonita",
+	// 		Preco:      39,
+	// 		Quantidade: 10},
+	// 	{"Tenis", "Conforatável", 89, 3},
+	// 	{"Fone", "Muito bom", 59, 2},
+	// }
+
+	db := conectaComBancoDeDados()
+
+	rows, err := db.Query("SELECT * FROM produtos")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	p := Produto{}
+
+	produtos := []Produto{}
+
+	for rows.Next() {
+		var id, quantidade int
+		var nome, descricao string
+		var preco float64
+
+		err = rows.Scan(&id, &nome, &descricao, &preco, &quantidade)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		p.Nome = nome
+		p.Descricao = descricao
+		p.Preco = preco
+		p.Quantidade = quantidade
+
+		produtos = append(produtos, p)
+
 	}
 
 	temp.ExecuteTemplate(w, "Index", produtos)
+	defer db.Close()
 }
