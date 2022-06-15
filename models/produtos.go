7885ef56bffa11213a01294484dae179a43a5144
Fabio Biasi Mello Rubim <fabiorubim@hotmail.com>
2022-06-15 01:52:36 -0300

    Deletando produtos e partials em Go.

--- models/produtos.go
+++ models/produtos.go
@@ -3,7 +3,7 @@ package models
 import "loja/db"
 
 type Produto struct {
-	id         int
+	Id         int
 	Nome       string
 	Descricao  string
 	Preco      float64
@@ -32,6 +32,7 @@ func BuscaTodosOsProdutos() []Produto {
 			panic(err.Error())
 		}
 
+		p.Id = id
 		p.Nome = nome
 		p.Descricao = descricao
 		p.Preco = preco
@@ -52,5 +53,16 @@ func CriarNovoProduto(nome, descricao string, preco float64, quantidade int) {
 		panic(err.Error())
 	}
 	insereDadosNoBanco.Exec(nome, descricao, preco, quantidade)
+	defer db.Close()
+}
+
+func DeletaProduto(id string) {
+	db := db.ConectaComBancoDeDados()
+	deletarOProduto, err := db.Prepare("DELETE FROM produtos WHERE id = $1")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	deletarOProduto.Exec(id)
 	defer db.Close()
 }
